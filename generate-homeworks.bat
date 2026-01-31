820171b140a404ea4eb7687003ef38f7fe0f6485
Nurmuhammad <nurmuhammad.youtub@gmail.com>
2026-02-01 03:03:28 +0500

    Changed

--- generate-homeworks.bat
+++ generate-homeworks.bat
@@ -15,35 +15,4 @@ if errorlevel 1 (
 
 echo.
 echo ✅ Homeworks JSON updated
-
-:: ---------------------------
-:: Pull remote changes (handle unrelated histories)
-:: ---------------------------
-git pull origin main --allow-unrelated-histories
-
-:: ---------------------------
-:: Stage all changes
-:: ---------------------------
-git add .
-
-:: ---------------------------
-:: Ask for commit message
-:: ---------------------------
-set /p commitmsg=Enter commit message: 
-
-:: ---------------------------
-:: Commit changes
-:: ---------------------------
-git commit -m "%commitmsg%"
-
-:: ---------------------------
-:: Push to GitHub
-:: ---------------------------
-git push origin main
-
-echo.
-echo ================================
-echo   All changes pushed to GitHub!
-echo   Your GitHub Pages site should update shortly.
-echo ================================
 pause
